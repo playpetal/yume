602f7d75c655d93293c26099204f2b7375981e80
dreamingkills <realrtfl@gmail.com>
2022-04-08 23:02:07 -0400

    (fix) remove oauth from context

--- api/context.ts
+++ api/context.ts
@@ -1,16 +1,13 @@
 import { db } from "./db";
 import { PrismaClient } from "@prisma/client";
-import OAuth from "discord-oauth2";
-import { discordOAuth2 } from "./graphql/util/auth/DiscordOAuth";
 import { Request } from "express";
 import { Redis } from "ioredis";
 import { redis } from "./lib/redis";
 
 export interface Context {
   db: PrismaClient;
-  discord: OAuth;
   req: Request;
   redis: Redis;
 }
 
-export const context = { db, discord: discordOAuth2, redis };
+export const context = { db, redis };
