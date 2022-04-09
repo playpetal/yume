6f22dd4238d48231a353972eb3dd52616ec43508
dreamingkills <realrtfl@gmail.com>
2022-04-08 21:53:28 -0400

    (refactor) add new user-facing errors

--- api/lib/error/index.ts
+++ api/lib/error/index.ts
@@ -1,3 +1,41 @@
+import { ApolloError } from "apollo-server-errors";
+
 export const NoPermissionError: Error = new Error(
   "You don't have permission to do that."
 );
+
+export class UnauthorizedError extends ApolloError {
+  isUserFacing: boolean = true;
+  name = "UnauthorizedError";
+
+  constructor() {
+    super("you need to be signed in to access that.", "UNAUTHORIZED");
+  }
+}
+
+export class InvalidInputError extends ApolloError {
+  isUserFacing: boolean = true;
+  name = "InvalidInputError";
+
+  constructor(message: string) {
+    super(message, "INVALID_INPUT");
+  }
+}
+
+export class DuplicateAccountError extends ApolloError {
+  isUserFacing: boolean = true;
+  name = "DuplicateAccountError";
+
+  constructor() {
+    super("you already have an account.", "DUPLICATE_ACCOUNT");
+  }
+}
+
+export class UsernameTakenError extends ApolloError {
+  isUserFacing: boolean = true;
+  name = "UsernameTakenError";
+
+  constructor() {
+    super("that username has already been taken.", "USERNAME_TAKEN");
+  }
+}
