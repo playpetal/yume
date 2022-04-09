5cf97d9f804d5ae35f3789c5de44f6580130da71
dreamingkills <realrtfl@gmail.com>
2022-04-08 22:40:18 -0400

    (refactor) Flag enum to own folder

--- api/graphql/account/index.ts
+++ api/graphql/account/index.ts
@@ -1,3 +1,4 @@
+export * from "./enum";
 export * from "./object";
 export * from "./mutation";
 export * from "./query";
--- api/graphql/account/object/index.ts
+++ api/graphql/account/object/index.ts
@@ -1,5 +1,4 @@
 export * from "./Account";
 export * from "./AccountStats";
-export * from "./Flag";
 export * from "./GTSStats";
 export * from "./WordsStats";
